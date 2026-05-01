01e10ecf21392ac298778f755cf6a5e9cbea1e56
Mouloud78 <mouloudouldali78@gmail.com>
2026-04-30 21:51:20 -0400

    investment input model added

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,15 +11,19 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
   entredInitialInvestment = '0';
   entredAnnualInvestment = '0';
   entredExpectedReturn = '5';
   entredDuration = '10';
 
   onSubmit() {
-    console.log(this.entredInitialInvestment);
-    console.log(this.entredAnnualInvestment);
-    console.log(this.entredExpectedReturn);
-    console.log(this.entredDuration);
+    this.calculate.emit({
+      initialInvestment: +this.entredInitialInvestment,
+      duration: +this.entredDuration,
+      expectedReturn: +this.entredExpectedReturn,
+      annualInvestment: +this.entredAnnualInvestment,
+    });
   }
 }
